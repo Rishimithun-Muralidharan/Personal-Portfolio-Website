da88288f0a7b480054d246d719ae003d3989e188
Rishimithun Muralidharan <rishimithun123@gmail.com>
2023-03-27 14:10:29 +0530

    finished navbar

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -1,7 +1,15 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faMedium,
+  faBehance,
+} from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -9,6 +17,81 @@ const Sidebar = () => (
       <img src={LogoS} alt="logo" />
       <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
     </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/rishimithun123/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            color="#4d4d4e"
+            className="anchor-icon"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/Rishimithun-Muralidharan"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            color="#4d4d4e"
+            className="anchor-icon"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://medium.com/@rishimithun123"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faMedium}
+            color="#4d4d4e"
+            className="anchor-icon"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.behance.net/rishimimuralid"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faBehance}
+            color="#4d4d4e"
+            className="anchor-icon"
+          />
+        </a>
+      </li>
+    </ul>
   </div>
 )
 export default Sidebar
